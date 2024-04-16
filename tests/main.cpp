c7c19332e4f32b1b695f75ebe15626e61704eb01
Arnold Zhou <magicarnold2022@gmail.com>
2024-04-16 17:48:03 +0800

    update tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,15 +2,53 @@
 
 #include "../include/termstyle.hpp"
 
-termstyle::PresetConfig preset_config = {
-    {"[ERROR] ", {termstyle::codes::BRIGHT, termstyle::codes::FOREGROUND_RED}, {termstyle::codes::RESTORE}},
-    {"\n", {}, {termstyle::codes::RESTORE}}};
+namespace ts = termstyle;
 
 int main()
 {
-    termstyle::addPreset("error", preset_config);
-    // termstyle::addPreset("error", preset_config); // throws PresetNameUsed
-    termstyle::print("error", "This is an error message printed using termstyle::print.");
-    termstyle::style("error") << "This is an error message printed using termstyle::style.";
+    ts::PresetConfig error_preset;
+    error_preset.prefix.prestyles = {ts::Codes::BRIGHT, ts::Codes::FOREGROUND_RED};
+    error_preset.prefix.text = "[ERROR] ";
+    error_preset.prefix.poststyles = {ts::Codes::BRIGHT_RESET};
+    error_preset.suffix.text = "\n";
+
+    ts::PresetConfig debug_preset = {
+        .prefix = {
+            .prestyles = {ts::Codes::DIM, ts::Codes::FOREGROUND_CYAN},
+            .text = "[DEBUG] ",
+            .poststyles = {ts::Codes::DIM_RESET}
+        },
+        .suffix = {
+            .text = "\n"
+        }
+    };
+
+    ts::PresetConfig input_preset = {
+        .prefix = {
+            .text = "Whatever you type will have a green background",
+        },
+        .suffix = {
+            .prestyles = {ts::Codes::FLASH},
+            .text = " >> ",
+            .poststyles = {ts::Codes::FLASH_RESET, ts::Codes::BACKGROUND_GREEN}
+        }
+    };
+
+    ts::addPreset("error", error_preset);
+    ts::print("error", "This is an error message printed using termstyle::print.");
+    ts::style("error") << "This is an error message printed using termstyle::style.";
+
+    ts::addPreset("debug", debug_preset);
+    ts::print("debug", "This is a debug message printed using termstyle::print.");
+    ts::style("debug") << "This is a debug message printed using termstyle::style.";
+
+    ts::config::trailing_restore = false;
+    ts::addPreset("input", input_preset);
+    char * input;
+    ts::print("input");
+    scanf("%s", input);
+    ts::style("input");
+    scanf("%s", input);
+    
     return 0;
 }
