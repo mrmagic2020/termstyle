7023a2c0f3e10d61f6a19cf7f9fe19ed5415691d
Arnold Zhou <magicarnold2022@gmail.com>
2024-04-16 15:39:10 +0800

    update docs

--- docs/html/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['stylestring_0',['StyleString',['../structtermstyle_1_1_style_string.html',1,'termstyle']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['termstyle_0',['termstyle',['../md__2_users_2arnold_8zhou_2_documents_2_v_s_c_2_commandline_2termstyle_2_r_e_a_d_m_e.html',1,'']]]
-];
